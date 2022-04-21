fadd9642690b5f18709aaba189d2b23aed93227c
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-21 17:56:06 -0600

    Eslint acoplation

--- src/components/App.js
+++ src/components/App.js
@@ -1,98 +1,127 @@
-import React, {useState} from "react";
-import Card from "./Card"
-import '../styles/App.css'
+import React, { useState } from 'react';
+import Card from './Card';
+import '../styles/App.css';
 
 export default function App() {
-    
-    const [values,setValues] = useState(shuffle([
-        {value: 1,number: 1,selected: false,},
-        {value: 1,number: 2,selected: false,},
-        {value: 2,number: 1,selected: false,},
-        {value: 2,number: 2,selected: false,},
-        {value: 3,number: 1,selected: false,},
-        {value: 3,number: 2,selected: false,},
-        {value: 4,number: 1,selected: false,},
-        {value: 4,number: 2,selected: false,},
-        {value: 5,number: 1,selected: false,},
-        {value: 5,number: 2,selected: false,},
-        {value: 6,number: 1,selected: false,},
-        {value: 6,number: 2,selected: false,},
-        {value: 7,number: 1,selected: false,},
-        {value: 7,number: 2,selected: false,},
-        {value: 8,number: 1,selected: false,},
-        {value: 8,number: 2,selected: false,},
-        {value: 9,number: 1,selected: false,},
-        {value: 9,number: 2,selected: false,},
-        
-    ]));
-
-    const [counter,setCounter] = useState(0);
-
-    const [selected, setSelected] = useState([]);
-
-    function addCount() {
-        setCounter(counter + 1);
+  function shuffle(array) {
+    let currentIndex = array.length;
+    let randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      // eslint-disable-next-line no-plusplus
+      currentIndex--;
+      // eslint-disable-next-line no-param-reassign
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
+    return array;
+  }
 
-    function remove(card) {
-        setValues(values.filter((element) => (element['value']!== card['value'])))
+  const [values, setValues] = useState(
+    shuffle([
+      { value: 1, number: 1, selected: false },
+      { value: 1, number: 2, selected: false },
+      { value: 2, number: 1, selected: false },
+      { value: 2, number: 2, selected: false },
+      { value: 3, number: 1, selected: false },
+      { value: 3, number: 2, selected: false },
+      { value: 4, number: 1, selected: false },
+      { value: 4, number: 2, selected: false },
+      { value: 5, number: 1, selected: false },
+      { value: 5, number: 2, selected: false },
+      { value: 6, number: 1, selected: false },
+      { value: 6, number: 2, selected: false },
+      { value: 7, number: 1, selected: false },
+      { value: 7, number: 2, selected: false },
+      { value: 8, number: 1, selected: false },
+      { value: 8, number: 2, selected: false },
+      { value: 9, number: 1, selected: false },
+      { value: 9, number: 2, selected: false },
+    ])
+  );
+
+  const [counter, setCounter] = useState(0);
+
+  const [selected, setSelected] = useState([]);
+
+  function addCount() {
+    setCounter(counter + 1);
+  }
+
+  function remove(card) {
+    setValues(values.filter((element) => element.value !== card.value));
+  }
+
+  function cardDeselect() {
+    setValues((val) =>
+      val.map((element) => ({
+        value: element.value,
+        number: element.number,
+        selected: false,
+      }))
+    );
+  }
+
+  function verifyGame() {
+    if (
+      selected[0].value === selected[1].value &&
+      selected[0].number !== selected[1].number
+    ) {
+      window.setTimeout(() => remove(selected[0]), 500);
+      window.setTimeout(() => remove(selected[1]), 500);
     }
+    setSelected([]);
+    window.setTimeout(() => cardDeselect(), 1000);
+  }
 
-    function verifyGame() {
-        if (selected[0]['value'] === selected[1]['value'] 
-        && selected[0]['number'] !== selected[1]['number']) {
-            window.setTimeout(() => remove(selected[0]),500)
-            window.setTimeout(() => remove(selected[1]),500)
-        }
-        setSelected([])
-        window.setTimeout(() => cardDeselect(),1000)
-        
-    }
+  function updateCard(newCard) {
+    setValues((val) =>
+      val.map((value) =>
+        value.value === newCard.value && value.number === newCard.number
+          ? newCard
+          : value
+      )
+    );
+  }
 
-    function cardSelect(card) {
-        updateCard({value: card.value, number: card.number, selected: !card.selected})
-        selected.push({value: card.value, number: card.number, selected: !card.selected})
-        selected.length===2? verifyGame(): null;
-    }
+  function cardSelect(card) {
+    updateCard({
+      value: card.value,
+      number: card.number,
+      selected: !card.selected,
+    });
+    selected.push({
+      value: card.value,
+      number: card.number,
+      selected: !card.selected,
+    });
+    if (selected.length === 2) verifyGame();
+  }
 
-    function updateCard(newCard) {
-        setValues(values => values.map(
-            (value,i) => value.value === newCard.value && value.number === newCard.number? 
-            newCard:value))
-    }
-
-    function cardDeselect() {
-        setValues(values => values.map(
-            (element) => ({value: element.value, number: element.number, selected: false})))
-    }
-
-    function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-        while (currentIndex != 0) {
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
-        return array;
-      }
-
-    return (
-        <div>
-            <div className="title">
-                <h1 className="textTitle">Memoria</h1>
-                <h5 className="textTitle">Contador de Clicks: {counter}</h5>
-            </div>
-            <div className="container">
-                {values.map(function (value, index) {
-                    return <Card key={index} card={value} fun={cardSelect} counter={addCount} selected={selected}/>
-                })
-                }
-            </div>
-            {
-                values.length === 0? <h1 className="textTitle">Juego Completado</h1> : <></>
-            }
-
-        </div>
-    )
+  return (
+    <div>
+      <div className="title">
+        <h1 className="textTitle">Memoria</h1>
+        <h5 className="textTitle">Contador de Clicks: {counter}</h5>
+      </div>
+      <div className="container">
+        {values.map((value, index) => (
+          <Card
+            key={index}
+            card={value}
+            fun={cardSelect}
+            counter={addCount}
+            selected={selected}
+          />
+        ))}
+      </div>
+      {values.length === 0 ? (
+        <h1 className="textTitle">Juego Completado</h1>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,29 @@
-import React from "react";
-import '../styles/Card.css'
-import "../fonts/Kidsco.otf";
+import React from 'react';
+import '../styles/Card.css';
+import '../fonts/Kidsco.otf';
 
 export default function Card(props) {
+  function getFlipped() {
+    return props.card.selected ? 'flip' : '';
+  }
 
-    function getFlipped() {
-        return props.card['selected']? "flip" : ""
-    }
-
-    return (
-        <div onClick={()=> {props.selected.length <2 ? props.fun(props.card) : null; props.counter();}}>
-            
-            <div className={"flip-card" }>
-                <div className={"flip-card-inner " + getFlipped()} >
-                    <div className="flip-card-front">
-                        <h1>?</h1>
-                    </div>
-                    <div className={"flip-card-back " + getFlipped()}>
-                    <h1>{props.card['value']}</h1>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      onClick={() => {
+        if (props.selected.length < 2) props.fun(props.card);
+        props.counter();
+      }}
+    >
+      <div className={'flip-card'}>
+        <div className={`flip-card-inner ${getFlipped()}`}>
+          <div className="flip-card-front">
+            <h1>?</h1>
+          </div>
+          <div className={`flip-card-back ${getFlipped()}`}>
+            <h1>{props.card.value}</h1>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
